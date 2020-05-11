9fe217c84424824a12c85e0d33afe1feb3cc8e6f
default-007 <brianokola@gmail.com>
2020-05-12 02:43:57 +0300

    Prepare Config file for deployment to heroku

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://default-007:expandebles7@localhost/thinkspace'
+    
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -20,7 +20,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
@@ -31,6 +31,7 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://default-007:expandebles7@localhost/thinkspace'
 
 config_options = {
 'development':DevConfig,
